1553f502015e7412e5c3811a51f1ae1292dd20fb
Abbey Yacoe <ayacoe@lsst.org>
2018-07-27 10:04:00 -0700

    Working sliders
    CSS comments to make clear which filter cooresponds to which color
    Clean up JS and get sliders working.
    Basic CSS to look closer to normal

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
+// Variables
 var zoom = 100;
+
+// Image Classes
+var u_filter = document.getElementsByClassName('u_filter')[0];
+var g_filter = document.getElementsByClassName('g_filter')[0];
+var r_filter = document.getElementsByClassName('r_filter')[0];
+var i_filter = document.getElementsByClassName('i_filter')[0];
+var z_filter = document.getElementsByClassName('z_filter')[0];
+var y_filter = document.getElementsByClassName('y_filter')[0];
+
+// Slider Values 
+var u_value = document.getElementById('u_value');
+var g_value = document.getElementById('g_value');
+var r_value = document.getElementById('r_value');
+var i_value = document.getElementById('i_value');
+var z_value = document.getElementById('z_value');
+var y_value = document.getElementById('y_value');
 
 
 function zoom_in() {
-	zoom+=5;
-	document.getElementsByClassName('u_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('g_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('r_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('i_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('z_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('y_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
+	u_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	g_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	r_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	i_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	z_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	y_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	zoom+=2;
 }
 
 function zoom_out() {
-	zoom-=5;
-	document.getElementsByClassName('u_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('g_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('r_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('i_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('z_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
-	document.getElementsByClassName('y_filter')[0].setAttribute('style', 'background-size: ' + zoom + '%');
+	zoom-=2;
+	u_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	g_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	r_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	i_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	z_filter.setAttribute('style', 'background-size: ' + zoom + '%');
+	y_filter.setAttribute('style', 'background-size: ' + zoom + '%');
 }
 
+// TODO turns off layer on check
 function check_toggle() {
   // If the checkbox is checked, display the output text
   if (checkBox.checked == true){
@@ -30,44 +48,43 @@ function check_toggle() {
   }
 }
 
-var blueSat = document.getElementById("blueSat");
-var blueBright = document.getElementById("blueBright");
 
-blueSat.addEventListener('change', function() {
-	document.getElementById('1').setAttribute("style", "-webkit-filter:saturate(" + this.value + ")");
-});
-blueBright.addEventListener('change', function() {
-	document.getElementById('1').setAttribute("style", "-webkit-filter:brightness(" + this.value + ")");
-});
+function reset() {
+	zoom = 100;
+	var button = document.getElementById('button'); 
+	button.addEventListener('click', function() {
+		u_filter.setAttribute("style", "-webkit-filter: brightness(100%);-webkit-filter: saturate(2.5); ");
+		g_filter.setAttribute("style", "-webkit-filter: brightness(100%);-webkit-filter: saturate(2.5); ");
+		r_filter.setAttribute("style", "-webkit-filter: brightness(100%);-webkit-filter: saturate(2.5); ");
+		i_filter.setAttribute("style", "-webkit-filter: brightness(100%);-webkit-filter: saturate(2.5); ");
+		z_filter.setAttribute("style", "-webkit-filter: brightness(100%);-webkit-filter: saturate(2.5); ");
+		y_filter.setAttribute("style", "-webkit-filter: brightness(100%);-webkit-filter: saturate(2.5); ");
+	});
+	zoom_in()
+}
 
-var greenSat = document.getElementById("greenSat");
-var greenBright = document.getElementById("greenBright");
-
-greenSat.addEventListener('change', function() {
-	document.getElementById('2').setAttribute("style", "-webkit-filter:saturate(" + this.value + ")");
-});
-greenBright.addEventListener('change', function() {
-	document.getElementById('2').setAttribute("style", "-webkit-filter:brightness(" + this.value + ")");
-});
- 
-var redSat = document.getElementById("redSat");
-var redBright = document.getElementById("redBright");
-
-redSat.addEventListener('change', function() {
-	document.getElementById('3').setAttribute("style", "-webkit-filter:saturate(" + this.value + ")");
-});
-redBright.addEventListener('change', function() {
-	document.getElementById('3').setAttribute("style", "-webkit-filter:brightness(" + this.value + ")");
+	
+// Update image values on slider change 
+u_value.addEventListener('change', function() {
+	u_filter.setAttribute('style', '-webkit-filter:saturate(' + this.value + ');-webkit-filter:brightness(' + this.value + ')');
 });
 
+g_value.addEventListener('change', function() {
+	g_filter.setAttribute('style', '-webkit-filter:saturate(' + this.value + ');-webkit-filter:brightness(' + this.value + ')');
+});
 
+r_value.addEventListener('change', function() {
+	r_filter.setAttribute('style', '-webkit-filter:saturate(' + this.value + ');-webkit-filter:brightness(' + this.value + ')');
+});
 
-var button = document.getElementById('button'); 
-button.addEventListener('click', function() {
-	document.getElementById('1').setAttribute("style", "-webkit-filter: brightness(100%);-webkit-filter: saturate(2.5); ");
-	// document.getElementById('1').style.backgroundColor = "transparent";
-	document.getElementById('2').setAttribute("style", "-webkit-filter: brightness(500%); -webkit-filter: saturate(17); ");
-	// document.getElementById('2').style.backgroundColor = "transparent !important";
-	document.getElementById('3').setAttribute("style", "-webkit-filter: saturate(4.4);-webkit-filter: brightness(320%); ");
-	// document.getElementById('3').style.backgroundColor = "transparent";});
+i_value.addEventListener('change', function() {
+	i_filter.setAttribute('style', '-webkit-filter:saturate(' + this.value + ');-webkit-filter:brightness(' + this.value + ')');
+});
+
+z_value.addEventListener('change', function() {
+	z_filter.setAttribute('style', '-webkit-filter:saturate(' + this.value + ');-webkit-filter:brightness(' + this.value + ')');
+});
+
+y_value.addEventListener('change', function() {
+	y_filter.setAttribute('style', '-webkit-filter:saturate(' + this.value + ');-webkit-filter:brightness(' + this.value + ')');
 });
